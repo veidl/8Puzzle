a9f623bb2c5f70f1ad409082cf3955405e3201bf
balji <baljinder.singh@stud.fh-campuswien.ac.at>
2021-05-02 14:57:49 +0200

    Add inversion check and manhattan distance

--- src/at/fhcampuswien/veidl/eightpuzzle/Etwas/Game.java
+++ src/at/fhcampuswien/veidl/eightpuzzle/Etwas/Game.java
@@ -1,8 +1,8 @@
 package at.fhcampuswien.veidl.eightpuzzle.Etwas;
 
-import at.fhcampuswien.veidl.eightpuzzle.Main;
-
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Game {
@@ -31,30 +31,54 @@ public class Game {
                 {4, 5, 6},
                 {7, 8, 0}};
 
-        this.startingNode = new Node(this.randomPuzzle, 0, calcHeuristic(this.randomPuzzle));
+        this.startingNode = new Node(this.randomPuzzle, 0, calcMisplacedHeuristic(this.randomPuzzle));
         this.queue = new PriorityQueue<>(Comparator.comparing(Node::calcF));
     }
 
     public void run() {
-        int[] emptyCor = getEmptySpaceCor(startingNode);
-        int g = 0;
+        if (isSolvable()) {
+            int[] emptyCor = getEmptySpaceCor(startingNode);
+            int g = 0;
 
-        moveLeft(startingNode, emptyCor[0], emptyCor[1], g);
-        resetEnv();
+            moveLeft(startingNode, emptyCor[0], emptyCor[1], g);
+            resetEnv();
 
-        moveRight(startingNode, emptyCor[0], emptyCor[1], g);
-        resetEnv();
+            moveRight(startingNode, emptyCor[0], emptyCor[1], g);
+            resetEnv();
 
-        moveTop(startingNode, emptyCor[0], emptyCor[1], g);
-        resetEnv();
+            moveTop(startingNode, emptyCor[0], emptyCor[1], g);
+            resetEnv();
 
-        moveBottom(startingNode, emptyCor[0], emptyCor[1], g);
-        resetEnv();
+            moveBottom(startingNode, emptyCor[0], emptyCor[1], g);
+            resetEnv();
 
+            System.out.println("p");
+            printNode(this.queue.peek().getNodeGrid());
+        }
 
-        System.out.println("p");
-        printNode(this.queue.peek().getNodeGrid());
+    }
 
+    private boolean isSolvable() {
+        int inversionCounter = 0;
+        List<Integer> invList = getListOfArray();
+
+        for (int i = 0; i < invList.size(); i++) {
+            for (int j = 0; j < invList.size(); j++) {
+                if (invList.get(i) > invList.get(j))
+                    inversionCounter++;
+            }
+        }
+        return inversionCounter % 2 == 0;
+    }
+
+    private List<Integer> getListOfArray() {
+        List<Integer> invList = new ArrayList<>();
+        for (int i = 0; i < randomPuzzle.length; i++) {
+            for (int j = 0; j < randomPuzzle.length; j++) {
+                invList.add(randomPuzzle[i][j]);
+            }
+        }
+        return invList;
     }
 
     public void resetEnv() { //Dumme scheiß methode
@@ -70,7 +94,7 @@ public class Game {
 //                {1, 2, 3},
 //                {4, 5, 6},
 //                {7, 0, 8}};
-        this.startingNode = new Node(this.randomPuzzle, 0, calcHeuristic(this.randomPuzzle));
+        this.startingNode = new Node(this.randomPuzzle, 0, calcMisplacedHeuristic(this.randomPuzzle));
     }
 
     public void addToQueue(Node node) {
@@ -100,8 +124,8 @@ public class Game {
         int left = grid[y][x - 1];
         grid[y][x - 1] = grid[y][x];
         grid[y][x] = left;
-        System.out.println(calcHeuristic(grid));
-        Node node = new Node(grid, g, calcHeuristic(grid));
+        System.out.println(calcMisplacedHeuristic(grid));
+        Node node = new Node(grid, g, calcMisplacedHeuristic(grid));
         printNode(node.getNodeGrid());
         addToQueue(node);
     }
@@ -116,8 +140,8 @@ public class Game {
         grid[y][x + 1] = grid[y][x];
         grid[y][x] = right;
 
-        System.out.println(calcHeuristic(grid));
-        Node node = new Node(grid, g, calcHeuristic(grid));
+        System.out.println(calcMisplacedHeuristic(grid));
+        Node node = new Node(grid, g, calcMisplacedHeuristic(grid));
         printNode(node.getNodeGrid());
         addToQueue(node);
     }
@@ -130,9 +154,9 @@ public class Game {
         int top = grid[y - 1][x];
         grid[y - 1][x] = grid[y][x];
         grid[y][x] = top;
-        System.out.println(calcHeuristic(grid));
+        System.out.println(calcMisplacedHeuristic(grid));
         printNode(grid);
-        Node node = new Node(grid, g, calcHeuristic(grid));
+        Node node = new Node(grid, g, calcMisplacedHeuristic(grid));
         addToQueue(node);
     }
 
@@ -144,14 +168,14 @@ public class Game {
         int bottom = grid[y + 1][x];
         grid[y + 1][x] = grid[y][x];
         grid[y][x] = bottom;
-        System.out.println(calcHeuristic(grid));
+        System.out.println(calcMisplacedHeuristic(grid));
         printNode(grid);
-        Node node = new Node(grid, g, calcHeuristic(grid));
+        Node node = new Node(grid, g, calcMisplacedHeuristic(grid));
         addToQueue(node);
     }
 
 
-    private int calcHeuristic(int[][] nodeGrid) {
+    private int calcMisplacedHeuristic(int[][] nodeGrid) {
         int misplacedCount = 0;
         for (int y = 0; y < nodeGrid.length; y++) {
             for (int x = 0; x < nodeGrid.length; x++) {
@@ -160,6 +184,35 @@ public class Game {
             }
         }
         return misplacedCount;
+    }
+
+    private int calcManhattanHeuristic(int[][] nodeGrid) {
+        int distance = 0;
+        for (int y = 0; y < nodeGrid.length; y++) {
+            for (int x = 0; x < nodeGrid.length; x++) {
+
+                //coordinates of current position in goalState
+                int p2X = findPointInGoalPuzzle(nodeGrid[y][x]).get(1);
+                int p2Y = findPointInGoalPuzzle(nodeGrid[y][x]).get(0);
+
+                distance += Math.abs(x - p2X) + Math.abs(y - p2Y);
+
+            }
+        }
+        return distance;
+    }
+
+    private List<Integer> findPointInGoalPuzzle(int point) {
+        List<Integer> cor = new ArrayList<>();
+        for (int i = 0; i < goalPuzzle.length; i++) {
+            for (int j = 0; j < goalPuzzle.length; j++) {
+                if (this.goalPuzzle[i][j] == point)
+                    cor.add(i);
+                cor.add(j);
+                return cor;
+            }
+        }
+        return cor;
     }
 
     private void printNode(int[][] currGrid) {
